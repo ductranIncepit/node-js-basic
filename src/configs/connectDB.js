e33d12f484a9804383e1d448e3c2adb6edadd8c9
Tran Duc <duc.tran@incepit.com>
2022-08-31 16:53:30 +0700

    End Project Node Basic

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,8 +1,9 @@
 // get the client
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
 // create the connection to database
-const connection = mysql.createConnection({
+
+const pool =  mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'root',
@@ -13,4 +14,4 @@ const connection = mysql.createConnection({
 
 
 
-export default connection;
+export default pool;
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,18 +1,27 @@
-import connection from '../configs/connectDB'
+import pool from '../configs/connectDB'
 
-let getHomepage = (req,res) => {
+let getHomepage = async (req,res) => {
     //logic
 let data =[];
-connection.query(
-  'SELECT * FROM `users`',
-  function(err, results, fields) {
-   results.map((row) =>  data.push(row));
-   return res.render('test/index.ejs',{dataUser: JSON.stringify(data)});
 
-  }
-);
+ const [rows, fields] = await pool.execute('SELECT * FROM users');
+ return res.render('index.ejs',{dataUser: rows})
+}
+
+let getDetailPage = async (req,res) => {
+    let userId = req.params.id;
+    let [user] = await pool.execute(`select * from users where id = ?`,[userId]);
+    return res.send(JSON.stringify(user));
+}
+
+let createNewUser = async (req,res) =>{
+    let {firstName,lastName,email,address} = req.body;
+    await pool.execute(`insert into users(firstName,lastName,email,address) values (?,?,?,?)`,[firstName,lastName,email,address])
+    return res.redirect('/');
 }
 
 module.exports = {
-    getHomepage
+    getHomepage,
+    getDetailPage,
+    createNewUser
 }
